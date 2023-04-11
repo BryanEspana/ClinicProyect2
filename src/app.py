8f50def023ace814ed266636d28b539213824e12
Bryan España <bryanespana21550@gmail.com>
2023-04-10 23:18:22 -0600

    Agregar enrutamiento a Login

--- src/app.py
+++ src/app.py
@@ -4,11 +4,12 @@ from config import config
 app=Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('auth/main.html')
 
-
+@app.route('/auth/login')
+def login():
+    return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
