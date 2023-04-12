5717800438e0bc180844a11b53ef616cc7316020
Bryan España <bryanespana21550@gmail.com>
2023-04-11 22:25:48 -0600

    Conexion con bases de datos
    conexión completa

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 class DevelopmentConfig():
-    DEBUG=True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:190319@localhost/proyect2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-config={
-    'development':DevelopmentConfig
+config = {
+    'development': DevelopmentConfig
 }
-     
---
+++ src/models.py
@@ -0,0 +1,91 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hospital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    direccion = db.Column(db.String(255))
+
+class Medico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    direccion = db.Column(db.String(255), nullable=False)
+    numero_colegiado = db.Column(db.String(50), nullable=False)
+    especialidad = db.Column(db.String(255), nullable=False)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
+
+    hospital = db.relationship('Hospital', backref=db.backref('medicos', lazy=True))
+
+class Paciente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    direccion = db.Column(db.String(255), nullable=False)
+    masa_corporal = db.Column(db.Float, nullable=False)
+    altura = db.Column(db.Float, nullable=False)
+    peso = db.Column(db.Float, nullable=False)
+    adicciones = db.Column(db.String(255))
+    enfermedades_hereditarias = db.Column(db.String(255))
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
+
+    hospital = db.relationship('Hospital', backref=db.backref('pacientes', lazy=True))
+
+class Diagnostico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'), nullable=False)
+    medico_id = db.Column(db.Integer, db.ForeignKey('medico.id'), nullable=False)
+    enfermedad = db.Column(db.String(255), nullable=False)
+    tratamiento = db.Column(db.String(255), nullable=False)
+    fecha_hora = db.Column(db.DateTime, nullable=False)
+    resultado = db.Column(db.String(255))
+
+    paciente = db.relationship('Paciente', backref=db.backref('diagnosticos', lazy=True))
+    medico = db.relationship('Medico', backref=db.backref('diagnosticos', lazy=True))
+
+class Transladomedico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    medico_id = db.Column(db.Integer, db.ForeignKey('medico.id'), nullable=False)
+    hospital_origen_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
+    hospital_destino_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
+    fecha_inicio = db.Column(db.DateTime, nullable=False)
+    fecha_fin = db.Column(db.DateTime, nullable=False)
+
+    medico = db.relationship('Medico', backref=db.backref('traslados', lazy=True))
+    hospital_origen = db.relationship('Hospital', foreign_keys=[hospital_origen_id], backref=db.backref('traslados_salientes', lazy=True))
+    hospital_destino = db.relationship('Hospital', foreign_keys=[hospital_destino_id], backref=db.backref('traslados_entrantes', lazy=True))
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+class Bitacora(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    accion = db.Column(db.String(255), nullable=False)
+    fecha_hora = db.Column(db.DateTime, nullable=False)
+
+    usuario = db.relationship('Usuario', backref=db.backref('bitacoras', lazy=True))
+
+class Especialidad(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+
+class Medico_Especialidad(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    medico_id = db.Column(db.Integer, db.ForeignKey('medico.id'), nullable=False)
+    especialidad_id = db.Column(db.Integer, db.ForeignKey('especialidad.id'), nullable=False)
+
+    medico = db.relationship('Medico', backref=db.backref('medico_especialidades', lazy=True))
+    especialidad = db.relationship('Especialidad', backref=db.backref('medico_especialidades', lazy=True))
+
+class Medicamentos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    cantidad = db.Column(db.Integer)
+    fecha_vencimiento = db.Column(db.Date)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'))
+    hospital = db.relationship("Hospital", backref="medicamentos")
