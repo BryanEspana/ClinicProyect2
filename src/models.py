fbb43c34d3e77a4ba6aa1bdd090d59cb16c0cd21
Bryan España <bryanespana21550@gmail.com>
2023-04-15 10:44:47 -0600

    Implementacion de modelos

--- src/models.py
+++ src/models.py
@@ -1,21 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+class Paciente(db.Model):
+    id_paciente = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    masa_corporal = db.Column(db.Float, nullable=False)
+    altura = db.Column(db.Float, nullable=False)
+    adicciones = db.Column(db.Text, nullable=False)
+    telefono = db.Column(db.String(15), nullable=False)
+    direccion = db.Column(db.Text, nullable=False)
 
-class Hospital(db.Model):
-    __tablename__ = 'hospitales'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    direccion = db.Column(db.String(200), nullable=False)
-    telefono = db.Column(db.String(20), nullable=False)
-    # Añade más columnas según tus necesidades
-    
+
 class Medico(db.Model):
-    __tablename__ = 'medicos'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    especialidad = db.Column(db.String(100), nullable=False)
-    telefono = db.Column(db.String(20), nullable=False)
-    # Añade más columnas según tus necesidades
-    
-# Añade tus otros modelos aquí
+    id_medico = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    direccion = db.Column(db.Text, nullable=False)
+    telefono = db.Column(db.String(15), nullable=False)
+    num_colegiado = db.Column(db.String(15), nullable=False)
+    especialidad = db.Column(db.String(40), nullable=False)
+    id_lugar = db.Column(db.Integer, db.ForeignKey('lugar.id_lugar'), nullable=False)
+
+
+class Enfermedad(db.Model):
+    id_enfermedad = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(15), nullable=False)
+
+
+class UtencilioMed(db.Model):
+    id_utencilio = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+
+
+class Lugar(db.Model):
+    id_lugar = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    localizacion = db.Column(db.Text, nullable=False)
+
+
+class Usuario(db.Model):
+    id_medico = db.Column(db.Integer, db.ForeignKey('medico.id_medico'), primary_key=True)
+    usuario = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
+
+
+class Inventario(db.Model):
+    id_inventario = db.Column(db.Integer, primary_key=True)
+    id_lugar = db.Column(db.Integer, db.ForeignKey('lugar.id_lugar'), nullable=False)
+    id_utencilio = db.Column(db.Integer, db.ForeignKey('utencilio_med.id_utencilio'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+    expiracion = db.Column(db.Date, nullable=False)
+
+
+class Historial(db.Model):
+    id_historial = db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.Date, nullable=False)
+    herencia = db.Column(db.Text, nullable=False)
+    tratamiento = db.Column(db.Text, nullable=False)
+    evolucion = db.Column(db.Text, nullable=False)
+    estado = db.Column(db.String(100), nullable=False)
+    comentario = db.Column(db.Text, nullable=False)
+    id_lugar = db.Column(db.Integer, db.ForeignKey('lugar.id_lugar'), nullable=False)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('paciente.id_paciente'), nullable=False)
+    id_medico = db.Column(db.Integer, db.ForeignKey('medico.id_medico'), nullable=False)
+    id_enfermedad = db.Column(db.Integer, db.ForeignKey('enfermedad.id_enfermedad'), nullable=False)
+
+class Visita(db.Model):
+    id_visita = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('paciente.id_paciente'), nullable=False)
+    id_medico = db.Column(db.Integer, db.ForeignKey('medico.id_medico'), nullable=False)
+    id_lugar = db.Column(db.Integer, db.ForeignKey('lugar.id_lugar'), nullable=False)
+
+    paciente = db.relationship('Paciente', backref='visitas')
+    medico = db.relationship('Medico', backref='visitas')
+    lugar = db.relationship('Lugar', backref='visitas')
